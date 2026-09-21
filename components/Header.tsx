3f6d3b977349b413930bc1df5b93c9465b4d0b4c
CHRISTIAN CHIKAODI OKORO <okoro.christianpeace@gmail.com>
2026-09-21 10:46:47 +0300

    feat: add artists and music catalogue pages

---
+++ components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="site-header"><a className="brand" href="/">SOULVILLE <span>RECORDS</span></a><nav><a href="/artists">Artists</a><a href="/music">Music</a><a href="/tour">Tour</a><a href="/about">About</a><a href="/contact">Contact</a></nav></header>;
+}
